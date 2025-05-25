a932c0b963f3bfa3e7bb2c58277d2d27e7d510f4
Oluwatosin Olajide <oluwatosinaina424@gmail.com>
2025-05-25 11:56:25 +0000

    Minor adjustment

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import torch, os, json
 from typing import Dict
 from torch import nn
+from huggingface_hub import HfApi
 
 def save_checkpoint(output_dir: str, model, optimizer, epoch: int, metrics: float, 
                    is_best: bool = False):
@@ -19,3 +20,19 @@ def save_checkpoint(output_dir: str, model, optimizer, epoch: int, metrics: floa
         print(f"\nSaving checkpoint for epoch {epoch+1}")
         
     torch.save(checkpoint, save_path)
+
+def push_to_hub():
+    # Initialize Hugging Face API
+    api = HfApi()
+
+    # Create repository (if it doesn't exist)
+    repo_id = "AfriMM/SiglipNllb"
+    api.create_repo(repo_id, exist_ok=True)
+
+    # Upload the weights file
+    api.upload_file(
+        path_or_fileobj="outputs/checkpoint_epoch_15.pth",
+        path_in_repo="model.pth",
+        repo_id=repo_id,
+        commit_message="Upload Siglip pytorch weights"
+    )
